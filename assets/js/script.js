395f890c1bbe1769002fded0a6fcb44ff6aa45e2
rona chen <rona.chen@hotmail.com>
2023-05-10 23:45:50 -0400

    wrapped code inside one function, and reformatted css file, and html file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,11 @@
-var currentDayEl = $('#currentDay');
+$(function () {
 
-function displayCurrentTime () {
-  var now = dayjs().format('dddd, MMMM D, YYYY h:mm A');
-  currentDayEl.text(now);
-}
-setInterval(displayCurrentTime, 1000);
+  // Updates the current date and time in the header every one second
+  function displayCurrentTime() {
+    var currentDayEl = $('#currentDay');
+    var now = dayjs().format('dddd, MMMM D, YYYY h:mm:ss A');
+    currentDayEl.text(now);
+  }
+  setInterval(displayCurrentTime, 1000);
 
-var hours =['09', '10', '11', '12', '13', '14', '15', '16', '17'];
+});
